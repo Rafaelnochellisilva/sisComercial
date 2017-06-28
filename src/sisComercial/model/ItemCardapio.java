4d485463699de4e28c6ce54d9caabffb8415740d
RafaelNochelli\Rafael Nochelli <rafaelnochellidasilva@gmail.com>
2017-06-28 16:45:03 -0300

    no message

--- src/sisComercial/model/ItemCardapio.java
+++ src/sisComercial/model/ItemCardapio.java
@@ -1,5 +1,64 @@
 package sisComercial.model;
 
+import java.util.UUID;
+
 public class ItemCardapio {
+	private String id;
+	private double preco;
+	private int quantidade;
+	private Cardapio cardapio;
+	private Pedido pedido;
+
+	public ItemCardapio() {
+		this.id = UUID.randomUUID().toString();
+	}
+
+	public ItemCardapio(double preco, int quantidade, Cardapio cardapio, Pedido pedido) {
+		this();
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.cardapio = cardapio;
+		this.pedido = pedido;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Cardapio getCardapio() {
+		return cardapio;
+	}
+
+	public void setCardapio(Cardapio cardapio) {
+		this.cardapio = cardapio;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
 
 }
--- src/sisComercial/model/Pedido.java
+++
@@ -1,5 +0,0 @@
-package sisComercial.model;
-
-public class Pedido {
-
-}
